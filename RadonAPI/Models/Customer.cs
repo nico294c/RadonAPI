b7a27d472227071f3cbd7ef6b666cccf99b81b74
nico294c <nico294c@easv365.dk>
2023-05-31 11:09:02 +0200

    BsonType Object ID for Customer ID

--- RadonAPI/Models/Customer.cs
+++ RadonAPI/Models/Customer.cs
@@ -6,6 +6,7 @@
     public class Customer
     {
         [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
         [BsonElement("username")]
