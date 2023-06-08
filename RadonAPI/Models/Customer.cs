cc82ed4c6ea47154d8858d525ff4d19c4a636844
nico294c <nico294c@easv365.dk>
2023-06-08 09:27:53 +0200

    Added cors and changed username to email

--- RadonAPI/Models/Customer.cs
+++ RadonAPI/Models/Customer.cs
@@ -9,8 +9,8 @@
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
-        [BsonElement("username")]
-        public string Username { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
 
         [BsonElement("password")]
         public string Password { get; set; }
